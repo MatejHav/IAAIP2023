Fix the annotations for the rotation augmentation. Now we rotate the mask directly.

culane/lane_dataset.py
@@ -4,6 +4,8 @@ import cv2
 import numpy as np
 import imgaug.augmenters as iaa
 from imgaug.augmenters import Resize
+import torchvision.transforms.functional as TF
+from scipy import ndimage
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 from torch.utils.data.dataset import Dataset
@@ -20,53 +22,6 @@ GROUND_TRUTH_GRID = (64, 160)
 # Based on: https://github.com/lucastabelini/LaneATT/tree/2f8583ba14eccba05e6779668bc3a38bc751984a
 #
 
-#\\\\Method 1 from https://stackoverflow.com/questions/61447973/how-to-calculate-positions-of-new-points-after-image-rotation
-def get_size_of_rotated_image(original_height, original_width, rotation_angle):
-    height_prime = abs(original_height * math.cos(rotation_angle)) + abs(original_width * math.sin(rotation_angle))
-    width_prime = abs(original_height * math.sin(rotation_angle)) + abs(original_width * math.cos(rotation_angle))
-    return height_prime, width_prime
-
-
-def rotate(pt, radians, origin):
-    x, y = pt
-    offset_x, offset_y = origin
-    adjusted_x = (x - offset_x)
-    adjusted_y = (y - offset_y)
-    cos_rad = math.cos(radians)
-    sin_rad = math.sin(radians)
-    qx = offset_x + cos_rad * adjusted_x + sin_rad * adjusted_y
-    qy = offset_y + -sin_rad * adjusted_x + cos_rad * adjusted_y
-    return qx, qy
-
-def rotate_point(pt, angle, origin, height, width):
-    x1, y1 = pt
-    x1_new, y1_new = rotate([x1, y1], angle, origin)
-    h_new, w_new = get_size_of_rotated_image(height, width, angle)
-    xoffset, yoffset = (w_new - width)/2, (h_new - height)/2
-    x1_new, y1_new = x1_new + xoffset, y1_new + yoffset
-    return x1_new, y1_new
-#\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-
-#Method2 - Similar approach -> Translating the point to a new coordinate system with the point of rotation being the origin
-def rotate_point2(point, angle, origin):
-    origin_x, origin_y = origin
-    rotation_matrix = np.array([[math.cos(angle), -math.sin(angle)],
-                                [math.sin(angle), math.cos(angle)]])
-
-    # Translate the point coordinates to the new coordinate system
-    translated_point = point - np.array([origin_x, origin_y])
-
-    # Apply the rotation matrix to the translated point
-    rotated_point = np.dot(rotation_matrix, translated_point)
-
-    # Translate the rotated point back to the original coordinate system
-    new_x = rotated_point[0] + origin_x
-    new_y = rotated_point[1] + origin_y
-
-    return new_x, new_y
-
-
 def adjust_ground_truth(img, item, augmentation, angle=None):
     """
     Shifts the points of the ground truth to correspond to the image after a horizontal flip is performed.
@@ -77,22 +32,6 @@ def adjust_ground_truth(img, item, augmentation, angle=None):
             match augmentation:
                 case 'flip':
                     lane[i] = (img.shape[1] - lane[i][0], lane[i][1])
-                case 'rotation':
-                    angle = np.deg2rad(angle)
-                    origin = (1600 / 2, 590 / 2)
-                    x_new, y_new = rotate_point2(lane[i], angle, origin)
-
-
-                    # \\\\\\\\\\\\old method\\\\\\\\\\
-                    # x = lane[i][0]
-                    # y = lane[i][1]
-                    # cosval = np.cos(angle)
-                    # sinval = np.sin(angle)
-                    # x_new = np.cos(angle) * x - np.sin(angle) * y
-                    # y_new = np.sin(angle) * x + np.cos(angle) * y
-                    # \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-                    lane[i] = (x_new, y_new)
                 case _:
                     raise Exception
 
@@ -398,19 +337,21 @@ class LaneDataset(Dataset):
         return mask[:, :, 0].astype(np.float32)
 
     def __getitem__(self, idx):
+        rotation_angle = None
+        img_rotation_flag = False
         item = self.dataset[idx]
         img = cv2.imread(item['path'])
         # For some reason cv2 resize wants to have width and then height
         original_shape = (img.shape[1], img.shape[0])
         #Iterate through the augmentations and adjust the ground truth per operation
         for aug in self.augmentations:
-            img = aug.augment_image(img)
             if iaa.flip.Fliplr == type(aug):
+                img = aug.augment_image(img)
                 adjust_ground_truth(img, item, 'flip')
-            # elif iaa.Affine == type(aug):
-                # angle = aug.rotate.value
-                # adjust_ground_truth(img, item, 'rotation', angle)
-
+            #We keep iaa.Affine in the arguments to the constructor just for consistency but we don't call this function anymore
+            elif iaa.Affine == type(aug):
+                rotation_angle = aug.rotate.value
+                img_rotation_flag = True
 
         # Resize image
         # img = cv2.resize(img, (self.img_w, self.img_h))
@@ -421,10 +362,8 @@ class LaneDataset(Dataset):
             y, x = self.resizing_coordinates[video_path]
             img = img[y:y+self.img_h, x:x+self.img_w]
         else:
-            # y = np.random.randint(0, img.shape[0] - self.img_h)
-            # x = np.random.randint(0, img.shape[1] - self.img_w)
-            y = 140
-            x = 200
+            y = np.random.randint(0, img.shape[0] - self.img_h)
+            x = np.random.randint(0, img.shape[1] - self.img_w)
             self.resizing_coordinates[video_path] = (y, x)
             img = img[y:y + self.img_h, x:x + self.img_w]
 
@@ -435,11 +374,16 @@ class LaneDataset(Dataset):
             img = (img - IMAGENET_MEAN) / IMAGENET_STD
         img = self.to_tensor(img.astype(np.float32))
         if not self.load_formatted:
-            transformed = self.transform_annotation(item)['lanes']
-            mask = cv2.resize(self.create_mask(transformed), original_shape)[y:y+self.img_h, x:x + self.img_w]
-            return img, transformed, mask, idx
-        mask = cv2.resize(self.create_mask(item['lanes']), original_shape)[y:y + self.img_h, x:x + self.img_w]
-        return img, item['lanes'], mask, idx
+            annotations = self.transform_annotation(item)['lanes']
+        else:
+            annotations = item['lanes']
+
+        mask = cv2.resize(self.create_mask(annotations), original_shape)[y:y + self.img_h, x:x + self.img_w]
+        if img_rotation_flag:
+            img = TF.rotate(img, rotation_angle)
+            rotated_mask = ndimage.rotate(mask, angle=rotation_angle, reshape=False)
+
+        return img, annotations, rotated_mask, idx
 
     def __len__(self):
         return len(self.dataset)

main.py
@@ -12,7 +12,8 @@ import numpy as np
 from tqdm import tqdm
 import cv2
 import imgaug.augmenters as iaa
-
+from scipy import ndimage
+import torchvision.transforms.functional as TF
 
 from models.backbone.backbone import Backbone
 
@@ -39,34 +40,6 @@ def rotate_mask(mask, angle):
     return mask
 
 
-def get_dataloader(split: str = 'train', batch_size: int = 30, subset=30):
-    root = './culane/data/'
-    # rotation_angle = random.randint(-45, 45)
-    augmentations = [
-        # {'name': 'HorizontalFlip', 'parameters': {'p': 0.5}},
-        {'name': 'Affine', 'parameters': {'rotate': 45}}
-    ]
-    dataset = LaneDataset(split=split, root=root, load_formatted=False, subset=subset, normalize=False, augmentations=augmentations)
-    it = dataset.__getitem__(33)
-    img = it[0]
-    mask = it[2]
-
-    rotated_mask = rotate_mask(mask, 45)
-    reshaped_mask = rotated_mask.reshape(320, 800, 3) / 255.0
-    reshaped_mask = np.mean(reshaped_mask, axis=2)
-
-    print_mask_on_image(img, reshaped_mask)
-
-    # loader = DataLoader(dataset=dataset,
-    #                           batch_size=batch_size,
-    #                           shuffle=False,  # Should shuffle the batches for each epoch
-    #                           worker_init_fn=_worker_init_fn_)
-    # return loader
-
-
-idx = 0
-
-
 def print_mask_on_image(img, mask):
     """
     Overlays an image from dataset.getitem() and its mask generated from the groundtruth. Used for testing purposes.
@@ -91,6 +64,34 @@ def print_mask_on_image(img, mask):
     plt.imshow(img_to_print)
     plt.axis('off')
     plt.show()
+
+
+def get_dataloader(split: str = 'train', batch_size: int = 30, subset=30):
+    root = './culane/data/'
+    rotation_angle = random.randint(-45, 45)
+    augmentations = [
+        {'name': 'HorizontalFlip', 'parameters': {'p': 0.5}},
+        {'name': 'Affine', 'parameters': {'rotate': rotation_angle}}
+    ]
+    dataset = LaneDataset(split=split, root=root, load_formatted=False, subset=subset, normalize=False,
+                          augmentations=augmentations)
+    it = dataset.__getitem__(44)
+    img = it[0]
+    mask = it[2]
+
+    print_mask_on_image(img, mask)
+
+    # loader = DataLoader(dataset=dataset,
+    #                           batch_size=batch_size,
+    #                           shuffle=False,  # Should shuffle the batches for each epoch
+    #                           worker_init_fn=_worker_init_fn_)
+    # return loader
+
+
+idx = 0
+
+
+
 
 
 if __name__ == '__main__':